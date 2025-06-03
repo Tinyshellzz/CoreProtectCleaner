70f0b5fe02706a5a6d791de4184826b98705998e
TinyShellzz <tinyshellzz@gmail.com>
2025-06-03 13:39:58 +0800

    增加打印错误功能

--- src/main/java/tcc/tinyshellzzz/coreProtectCleaner/tasks/CoCleanTask.java
+++ src/main/java/tcc/tinyshellzzz/coreProtectCleaner/tasks/CoCleanTask.java
@@ -1,11 +1,24 @@
 package tcc.tinyshellzzz.coreProtectCleaner.tasks;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import static tcc.tinyshellzzz.coreProtectCleaner.ObjectPool.coCleanService;
 
 public class CoCleanTask implements Runnable {
     public void run() {
-        coCleanService.clean();
+        try {
+            coCleanService.clean();
+        } catch (RuntimeException e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String sStackTrace = sw.toString();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + sStackTrace);
+        }
     }
 }
